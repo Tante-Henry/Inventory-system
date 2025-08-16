21b900e2fc947a26e9d8b5de81ba37104bf917dc
Tante-Henry <69560424+Tante-Henry@users.noreply.github.com>
2025-08-16 01:35:53 +0100

    Add item view templates with shared form partial

--- app/views/items/edit.php
+++ app/views/items/edit.php
@@ -6,12 +6,7 @@
 <body>
 <h1>Edit Item</h1>
 <form method="POST" action="/item/update/<?php echo $item->id; ?>">
-    <label>Name: <input type="text" name="name" value="<?php echo htmlspecialchars($item->name); ?>"></label><br>
-    <label>Quantity: <input type="number" name="quantity" value="<?php echo (int)$item->quantity; ?>"></label><br>
-    <label>Price:
-        <input type="number" step="0.01" name="price" value="<?php echo htmlspecialchars($item->price); ?>">
-    </label><br>
-    <button type="submit">Update</button>
+    <?php include __DIR__ . '/form.php'; ?>
 </form>
 </body>
 </html>
--- app/views/items/show.php
+++ app/views/items/show.php
@@ -5,7 +5,14 @@
 </head>
 <body>
 <h1><?php echo htmlspecialchars($item->name); ?></h1>
-<p>Quantity: <?php echo (int)$item->quantity; ?></p>
-<p>Price: <?php echo htmlspecialchars($item->price); ?></p>
+<ul>
+    <li>ID: <?php echo (int)$item->id; ?></li>
+    <li>Quantity: <?php echo (int)$item->quantity; ?></li>
+    <li>Price: <?php echo htmlspecialchars($item->price); ?></li>
+</ul>
+<p>
+    <a href="/item/edit/<?php echo $item->id; ?>">Edit</a>
+    <a href="/item/index">Back to List</a>
+</p>
 </body>
 </html>
